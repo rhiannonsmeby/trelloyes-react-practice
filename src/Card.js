3a665f73b605bfd48827a6dcc40f5488e5188ace
Rhiannon Smeby <smebyrhi@gmail.com>
2020-11-30 14:07:58 -0600

    added component files

---
+++ src/Card.js
@@ -0,0 +1,11 @@
+function Card(props) {
+    const title = ${props.Card};
+    const content = 
+    return (
+        <div className="Card">
+            <button type="button">delete</button>
+            <h3>Second card</h3>
+            <p>lorem ipsum</p>
+          </div>
+    )
+}
